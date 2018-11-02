85d3ac757778373857ba5c60fbfe7d8e7a2acfd4
Reginald Lamar Bramber II <architekespy@Reginalds-MacBook-Pro.local>
2018-11-02 15:35:58 -0400

    can not figure anything else out

--- src/App.js
+++ src/App.js
@@ -9,10 +9,12 @@ import Button from 'react-toolbox/lib/button/Button';
 class App extends Component {
   render() {
     return (
+      
       <ThemeProvider theme={theme}>
       <div className="App">
-      <h1>Gucci Mane</h1>
+      <h1>click the button below</h1>
       <Button label="Select" raised primary />
+      
       </div>
   </ThemeProvider>
   
